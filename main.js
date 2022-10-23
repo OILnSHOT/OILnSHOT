9f7a7f83d4267b0d430be1cc0e45512a638b11ab
OILnSHOT <OILnSHOT@users.noreply.github.com>
2022-10-23 21:52:17 +0200

    asd

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-pogoda(document.getElementById("main__searchbar").value);
+if (localStorage.getItem("miasto") != null) {
+  miasto = localStorage.getItem("miasto");
+  document.getElementById("main__searchbar").value = miasto;
+}
+pogoda(miasto);
+// pogoda(document.getElementById("main__searchbar").value);
 document.getElementById("main__searchbar").addEventListener("change", () => {
+  localStorage.setItem(
+    "miasto",
+    document.getElementById("main__searchbar").value
+  );
   pogoda(document.getElementById("main__searchbar").value);
 });
 
@@ -10,7 +19,11 @@ async function pogoda(miasto) {
   const data = await wynik.json();
   console.log(data);
   document.getElementById("main__city").innerHTML = data.name;
-  document.getElementById("main__temp").innerHTML = data.main.temp + "°C";
+  if (document.getElementById("main__searchbar").value == "Auschwitz") {
+    document.getElementById("main__temp").innerHTML = "9999" + "°C";
+  } else {
+    document.getElementById("main__temp").innerHTML = data.main.temp + "°C";
+  }
   document.getElementById("main__hum").innerHTML = data.main.humidity + "%";
   document.getElementById("main__wind").innerHTML = data.wind.speed + " m/s";
   document.getElementById("main__pre").innerHTML = data.main.pressure + " hPa";
@@ -21,17 +34,12 @@ function currentTime() {
   let hour = date.getHours();
   let minute = date.getMinutes();
   let second = date.getSeconds();
-  let PMorAM = "AM";
-
-  if (hour > 12) {
-    PMorAM = "PM";
-  }
 
   hour = hour < 10 ? "0" + hour : hour;
   minute = minute < 10 ? "0" + minute : minute;
   second = second < 10 ? "0" + second : second;
 
-  let time = hour + ":" + minute + ":" + second + " " + PMorAM;
+  let time = hour + ":" + minute + ":" + second;
 
   document.getElementById("main__clock").innerText = time;
   let wynik = setTimeout(function () {
